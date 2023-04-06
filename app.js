775d8589440facff5449ded10e108cc97406d6af
Jongwoo Baek <qorwhddn1234@gmail.com>
2023-04-06 18:47:04 +0900

    add click event to game img

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ function startGame() {
   mainImg.classList.toggle("hidden");
   container.classList.toggle("hidden");
 
-  for (const img of gameImg) {
-    img.classList.toggle("hidden");
-  }
+  //   for (const img of gameImg) {
+  //     img.classList.toggle("hidden");
+  //   }
 
   for (const el of square) {
     el.classList.toggle("hidden");
@@ -27,3 +27,15 @@ function startGame() {
 
 startBtn.addEventListener("click", startGame);
 reStartBtn.addEventListener("click", startGame);
+square[0].addEventListener("click", () => {
+  gameImg[0].classList.toggle("hidden");
+});
+square[1].addEventListener("click", () => {
+  gameImg[1].classList.toggle("hidden");
+});
+square[2].addEventListener("click", () => {
+  gameImg[2].classList.toggle("hidden");
+});
+square[3].addEventListener("click", () => {
+  gameImg[3].classList.toggle("hidden");
+});
