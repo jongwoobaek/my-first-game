118c5ba09a53209ea27c13ebdd8e2823c087892c
Jongwoo Baek <qorwhddn1234@gmail.com>
2023-04-06 15:35:32 +0900

    complete start-game

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-alert("I am js!");
+const startBtn = document.querySelector("#start-btn");
+const reStartBtn = document.querySelector("#re-start-btn");
+const mainImg = document.querySelector("#main-img");
+const square = document.querySelectorAll(".square");
+const container = document.querySelector("#container");
+const gameImg = document.querySelectorAll(".game-img");
+const h3 = document.querySelectorAll("h3");
+
+function startGame() {
+  startBtn.classList.toggle("hidden");
+  reStartBtn.classList.toggle("hidden");
+  mainImg.classList.toggle("hidden");
+  container.classList.toggle("hidden");
+
+  for (const img of gameImg) {
+    img.classList.toggle("hidden");
+  }
+
+  for (const el of square) {
+    el.classList.remove("hidden");
+  }
+
+  for (const el of h3) {
+    el.classList.remove("hidden");
+  }
+}
+
+startBtn.addEventListener("click", startGame);
