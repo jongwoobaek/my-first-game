e8ebca7da7d503ba8348e7fed21d0c848a0daffa
Jongwoo Baek <qorwhddn1234@gmail.com>
2023-04-07 01:30:25 +0900

    draft of game logic

--- app.js
+++ app.js
@@ -16,13 +16,6 @@ const mainH1 = document.querySelector("#main-h1");
 const endH1 = document.querySelector("#endGame-h1");
 
 // function
-// function toggleHidden() {
-//   startBtn.classList.toggle("hidden");
-//   reStartBtn.classList.toggle("hidden");
-//   mainImg.classList.toggle("hidden");
-//   container.classList.toggle("hidden");
-// }
-
 function startGame() {
   startBtn.classList.toggle("hidden");
   reStartBtn.classList.toggle("hidden");
@@ -56,7 +49,9 @@ function startGame() {
     } else {
       return;
     }
-  }, 3000);
+  }, 5000);
+
+  return;
 }
 
 // 리셋버튼을 두개를 만들어야 할듯..
@@ -99,8 +94,50 @@ startBtn.addEventListener("click", startGame);
 reStartBtn.addEventListener("click", reStartGame);
 reStartBtn2.addEventListener("click", reStartGame2);
 
-for (let i = 0; i < square.length; i++) {
-  square[i].addEventListener("click", () => {
-    gameImg[i].classList.toggle("hidden");
-  });
-}
+const checkArr = [];
+let resultArr;
+
+square[0].addEventListener("click", () => {
+  gameImg[0].classList.toggle("hidden");
+
+  checkArr.push(gameImg[0].alt);
+
+  setTimeout(() => {
+    if (checkArr[0] !== checkArr[1]) {
+      gameImg[0].classList.toggle("hidden");
+    } else {
+      return;
+    }
+  }, 1500);
+});
+
+square[1].addEventListener("click", () => {
+  gameImg[1].classList.toggle("hidden");
+
+  checkArr.push(gameImg[1].alt);
+});
+
+square[2].addEventListener("click", () => {
+  gameImg[2].classList.toggle("hidden");
+
+  checkArr.push(gameImg[2].alt);
+});
+
+square[3].addEventListener("click", () => {
+  gameImg[3].classList.toggle("hidden");
+
+  checkArr.push(gameImg[3].alt);
+});
+
+// for (let i = 0; i < square.length; i++) {
+//   square[i].addEventListener("click", () => {
+//     gameImg[i].classList.toggle("hidden");
+//     checkArr.push(gameImg[i].alt);
+//   });
+// }
+
+// if (!checkArr[0] === checkArr[1]) {
+//   setTimeout(() => {
+//     gameImg[i].classList.toggle("hidden");
+//   }, 0);
+// }
