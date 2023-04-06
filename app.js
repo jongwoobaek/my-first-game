8fcd61b9c728422d75618fce48b0f1cb39774289
Jongwoo Baek <qorwhddn1234@gmail.com>
2023-04-06 18:55:26 +0900

    add function reStartGame

--- app.js
+++ app.js
@@ -12,10 +12,6 @@ function startGame() {
   mainImg.classList.toggle("hidden");
   container.classList.toggle("hidden");
 
-  //   for (const img of gameImg) {
-  //     img.classList.toggle("hidden");
-  //   }
-
   for (const el of square) {
     el.classList.toggle("hidden");
   }
@@ -25,17 +21,30 @@ function startGame() {
   }
 }
 
+function reStartGame() {
+  startBtn.classList.toggle("hidden");
+  reStartBtn.classList.toggle("hidden");
+  mainImg.classList.toggle("hidden");
+  container.classList.toggle("hidden");
+
+  for (const el of square) {
+    el.classList.toggle("hidden");
+  }
+
+  for (const el of h3) {
+    el.classList.toggle("hidden");
+  }
+
+  for (const img of gameImg) {
+    img.className = "game-img hidden";
+  }
+}
+
 startBtn.addEventListener("click", startGame);
-reStartBtn.addEventListener("click", startGame);
-square[0].addEventListener("click", () => {
-  gameImg[0].classList.toggle("hidden");
-});
-square[1].addEventListener("click", () => {
-  gameImg[1].classList.toggle("hidden");
-});
-square[2].addEventListener("click", () => {
-  gameImg[2].classList.toggle("hidden");
-});
-square[3].addEventListener("click", () => {
-  gameImg[3].classList.toggle("hidden");
-});
+reStartBtn.addEventListener("click", reStartGame);
+
+for (let i = 0; i < square.length; i++) {
+  square[i].addEventListener("click", () => {
+    gameImg[i].classList.toggle("hidden");
+  });
+}
