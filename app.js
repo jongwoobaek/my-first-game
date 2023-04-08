200384b171ea7810e77f82a992b96a16fed6730d
Jongwoo Baek <qorwhddn1234@gmail.com>
2023-04-09 00:17:37 +0900

    add game logic

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const endH1 = document.querySelector("#endGame-h1");
 const checkArr = [];
 const resultArr = [];
 let timerId;
+let timerId2;
 let clickCounter = 0;
 const checkId = [];
 
@@ -51,36 +52,29 @@ function startGame() {
   resultArr.splice(0);
 
   changeNumOfFruits();
+
   setInterval(changeNumOfFruits, 0);
 
   timerId = setTimeout(() => {
-    if (resultArr.length === 4) {
-      clearTimeout(timerId);
-      alert("🎉");
-      location.reload();
+    if (startBtn.classList.value && mainImg.classList.value) {
+      reStartBtn.classList.toggle("hidden");
+      reStartBtn2.classList.toggle("hidden");
+      mainH1.classList.toggle("hidden");
+      endH1.classList.toggle("hidden");
+      gameOverImg.classList.toggle("hidden");
+      container.style.display = "none";
+      checkArr.splice(0);
+      clickCounter = 0;
 
-      return;
-    } else {
-      if (startBtn.classList.value && mainImg.classList.value) {
-        reStartBtn.classList.toggle("hidden");
-        reStartBtn2.classList.toggle("hidden");
-        mainH1.classList.toggle("hidden");
-        endH1.classList.toggle("hidden");
-        gameOverImg.classList.toggle("hidden");
-        container.style.display = "none";
-        checkArr.splice(0);
-        clickCounter = 0;
-
-        for (const el of square) {
-          el.classList.toggle("hidden");
-        }
-
-        for (const img of gameImg) {
-          img.className = "game-img hidden";
-        }
-      } else {
-        return;
+      for (const el of square) {
+        el.classList.toggle("hidden");
       }
+
+      for (const img of gameImg) {
+        img.className = "game-img hidden";
+      }
+    } else {
+      return;
     }
   }, 5000);
 }
@@ -145,11 +139,60 @@ startBtn.addEventListener("click", startGame);
 reStartBtn.addEventListener("click", reStartGame);
 reStartBtn2.addEventListener("click", reStartGame2);
 
+// for (let i = 0; i < square.length; i++) {
+//   square[i].addEventListener("click", () => {
+//     if (
+//       checkId[clickCounter - 1] !== Number(gameImg[i].id) &&
+//       gameImg[i].classList[1]
+//     ) {
+//       let imgId = Math.random();
+
+//       checkId.push(imgId);
+//       gameImg[i].id = imgId;
+
+//       gameImg[i].classList.toggle("hidden");
+
+//       checkArr.push(gameImg[i].alt);
+
+//       clickCounter++;
+//       setTimeout(() => {
+//         if (
+//           checkArr.length < 2 ||
+//           checkArr[clickCounter - 1] !== checkArr[clickCounter - 2]
+//         ) {
+//           gameImg[i].classList.toggle("hidden");
+//           checkArr.splice(0);
+//           clickCounter = 0;
+
+//           checkId.splice(0);
+//           gameImg[i].id = "";
+//         }
+
+//         if (
+//           checkArr.length >= 2 &&
+//           checkArr[clickCounter - 1] === checkArr[clickCounter - 2]
+//         ) {
+//           resultArr.push(checkArr[clickCounter - 1]);
+//           //   clickCounter = 0;
+//           //   checkArr.splice(0);
+//           // resultArr.push(checkArr[clickCounter - 2]);
+
+//           return;
+//         }
+//       }, 650);
+//     } else {
+//       return;
+//     }
+//   });
+// }
+
+//실험용
 for (let i = 0; i < square.length; i++) {
   square[i].addEventListener("click", () => {
     if (
       checkId[clickCounter - 1] !== Number(gameImg[i].id) &&
-      gameImg[i].classList[1]
+      gameImg[i].classList[1] &&
+      checkId.length < 2
     ) {
       let imgId = Math.random();
 
@@ -161,36 +204,69 @@ for (let i = 0; i < square.length; i++) {
       checkArr.push(gameImg[i].alt);
 
       clickCounter++;
-      setTimeout(() => {
-        if (
-          checkArr.length < 2 ||
-          checkArr[clickCounter - 1] !== checkArr[clickCounter - 2]
-        ) {
-          gameImg[i].classList.toggle("hidden");
+
+      if (checkArr[0] !== checkArr[1] && checkArr.length >= 2) {
+        clearTimeout(timerId2);
+
+        setTimeout(() => {
+          for (const img of gameImg) {
+            img.className = "game-img hidden";
+            img.id = "";
+          }
+
           checkArr.splice(0);
           clickCounter = 0;
 
           checkId.splice(0);
           gameImg[i].id = "";
-        }
+        }, 180);
+      } else {
+        timerId2 = setTimeout(() => {
+          if (
+            checkArr.length < 2 ||
+            checkArr[clickCounter - 1] !== checkArr[clickCounter - 2]
+          ) {
+            //   clearTimeout(timerId2);
 
-        if (
-          checkArr.length >= 2 &&
-          checkArr[clickCounter - 1] === checkArr[clickCounter - 2]
-        ) {
-          resultArr.push(checkArr[clickCounter - 1]);
-          // resultArr.push(checkArr[clickCounter - 2]);
+            gameImg[i].classList.toggle("hidden");
+            checkArr.splice(0);
+            clickCounter = 0;
 
-          return;
-        }
-      }, 750);
+            checkId.splice(0);
+            gameImg[i].id = "";
+          }
+
+          if (
+            checkArr.length >= 2 &&
+            checkArr[clickCounter - 1] === checkArr[clickCounter - 2]
+          ) {
+            resultArr.push(checkArr[clickCounter - 1]);
+
+            checkId.splice(0);
+
+            if (resultArr.length === 4) {
+              clearTimeout(timerId);
+              alert("🎉");
+              location.reload();
+            }
+
+            return;
+          }
+        }, 600);
+      }
+
+      //   if (checkArr[0] !== checkArr[1] && checkArr.length >= 2) {
+      //     // clearTimeout(timerId2);
+      //     for (const img of gameImg) {
+      //       img.className = "game-img hidden";
+      //       img.id = "";
+      //     }
+      //   }
     } else {
       return;
     }
   });
 }
-
-//실험용
 
 // function changeImg() {
 //   for (let i = 0; i < square.length; i++) {
